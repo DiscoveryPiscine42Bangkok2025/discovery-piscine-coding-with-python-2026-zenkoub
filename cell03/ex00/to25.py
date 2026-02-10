e8fd7bb8504fe0a5ced1a182ae0cb102ca0ae807
zenkoub <koobun1234@gmail.com>
2026-02-10 12:48:29 +0700

    submit: cell3.0

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,6 @@
+user_input = int(input("Enter a number less than 25 :\n"))
+if user_input > 25:
+   print("Error")
+else:
+   for i in range(int(user_input), 26):
+       print(f"Inside the loop, my variable is {i}")
