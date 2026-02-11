221fbec448fe8d0c9303e9350182f5c84a54cb4a
zenkoub <koobun1234@gmail.com>
2026-02-11 20:41:28 +0700

    submit: cell6.2

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+def downcase_it(user_input):
+    return user_input.lower()
+if len(sys.argv) == 1:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        print(downcase_it(arg))
