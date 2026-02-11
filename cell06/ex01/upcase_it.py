dba0fc50a44b18d10fc7e24f35b8f80fb11639c1
zenkoub <koobun1234@gmail.com>
2026-02-11 20:30:11 +0700

    submit: cell6.1

--- cell06/ex01/upcase_it.py
+++ cell06/ex01/upcase_it.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-def upcase_it():
-    import sys
-    args = sys.argv[1:]
-    if len(args) == 0:
-        print("none")
-    else:
-        args_upper = [arg.upper() for arg in args]
-        print(" ".join(args_upper))
-upcase_it()
+def upcase_it(input):
+    return input.upper()
+print(upcase_it("hello"))
